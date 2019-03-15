8f31319e54cc010df68d08beceb9941bdf6e4dc1
mohragk <sandervermeer1@gmail.com>
2019-03-15 22:07:57 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-smoothscroll/precache-manifest.129ddfe44c6ef53f02679fc822659117.js"
+  "/react-smoothscroll/precache-manifest.915f4904b1ee14a2878c7cf6dddb94f0.js"
 );
 
 workbox.clientsClaim();
